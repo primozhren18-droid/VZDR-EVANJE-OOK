ba0a541f722dcb5c21593f5189fc980391e30f98
primozhren18-droid <primozhren18@gmail.com>
2026-01-22 20:28:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,26 @@
-const CACHE = "vzdrzevanje-ook-v2";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./db.js","./manifest.json"];
+const CACHE = "vzdrzevanje-ook-v5";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./db.js",
+  "./manifest.json",
+];
 
 self.addEventListener("install", (e)=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener("activate", (e)=>e.waitUntil(self.clients.claim()));
+
+self.addEventListener("activate", (e)=>{
+  e.waitUntil((async ()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)));
+    await self.clients.claim();
+  })());
+});
+
 self.addEventListener("fetch", (e)=>{
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request).catch(()=>caches.match("./index.html")))
