414ec260c75edc3666c67a6f5c9a93576c881d56
primozhren18-droid <primozhren18@gmail.com>
2026-01-22 21:49:45 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "PASTE",
+  authDomain: "PASTE",
+  projectId: "PASTE",
+  storageBucket: "PASTE",
+  messagingSenderId: "PASTE",
+  appId: "PASTE"
+};
+
+export const fbApp = initializeApp(firebaseConfig);
+export const auth = getAuth(fbApp);
+export const db = getFirestore(fbApp);
+export const storage = getStorage(fbApp);
