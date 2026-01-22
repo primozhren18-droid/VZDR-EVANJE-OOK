db68aabba952a3656b345f09ad3abe438bc91fc9
primozhren18-droid <primozhren18@gmail.com>
2026-01-22 18:55:52 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "vzdrzevanje-ook-v2";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./db.js","./manifest.json"];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e)=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(()=>caches.match("./index.html")))
+  );
+});
