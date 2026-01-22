c99591bc93284a5fdf8c8e63403446e6c6f91dc0
primozhren18-droid <primozhren18@gmail.com>
2026-01-22 23:18:15 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDwyF6wlKxejpZOdvYea5JF5y5CCzkf_As",
+  apiKey: "AIzaSyDwyF6wLkxeipZ0dyYea5JF5y5Cczkf_As",
   authDomain: "vzdr-evanje-ook.firebaseapp.com",
   projectId: "vzdr-evanje-ook",
   storageBucket: "vzdr-evanje-ook.firebasestorage.app",
   messagingSenderId: "116508236756",
-  appId: "1:116508236756:web:9d6870e996f192e6251441",
-  measurementId: "G-LN8TCJTGQM"
+  appId: "1:116508236756:web:9d6870e996f192e6251441"
 };
+
+export const fbApp = initializeApp(firebaseConfig);
+export const auth = getAuth(fbApp);
+export const db = getFirestore(fbApp);
+export const storage = getStorage(fbApp);
